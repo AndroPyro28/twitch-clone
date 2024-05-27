3d3ad8c013883a6769e64ab7defd1e51b51f4f45
AndroPyro28 <menandroeugenio1028@gmail.com>
2024-05-28 00:43:17 +0800

    save

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,17 +1,27 @@
 import { withAuth } from "@/lib/auth";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { RedirectType, redirect } from "next/navigation";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+// custom auth route handlers
 
-export const GET = withAuth(
-  async ({currentUser,}) => {
+// export const GET = withAuth(
+//   async ({currentUser,}) => {
+     // If there is no signed in user, this will return a 404 error
+
+//     console.log(currentUser)
+
+     // Add your Route Handler logic here
+
+//     return NextResponse.json({ message: "Hello world!" });
+//   },
+//   { requiredRole: [] }
+// );
+
+export const GET  = (req: NextRequest) => {
+
     // If there is no signed in user, this will return a 404 error
-
-    console.log(currentUser)
 
     // Add your Route Handler logic here
 
     return NextResponse.json({ message: "Hello world!" });
-  },
-  { requiredRole: [] }
-);
+  }
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,24 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-export default clerkMiddleware({
-  
+import { clerkMiddleware, createRouteMatcher, clerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+export default clerkMiddleware(async (auth, req,) => {
+  if (isProtectedRoute(req)) {
+    // Add your middleware logic here
+    const {userId, } = auth()
+
+    if(!userId) {
+      return NextResponse.json({message: "Unauthorized: Login Required"}, {status: 401})
+    } 
+    const user = await clerkClient.users.getUser(userId);
+    if(!user) {
+      return NextResponse.json({message: "Unauthorized: Login Required"}, {status: 401})
+    } 
+  }
 });
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+const isProtectedRoute = createRouteMatcher([
+  // '/api/webhooks/clerk',
+  '/api/webhooks/livekit',
+]);
